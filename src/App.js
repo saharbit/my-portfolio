cf5538e4c9b9f5d88bacf211d9c52cb32e9e3f6f
Sahar <sahar.bit@gmail.com>
2020-09-12 13:24:48 +0300

    Background gradient change

--- src/App.js
+++ src/App.js
@@ -9,18 +9,14 @@ function App() {
             <div className="container mx-auto flex flex-col px-2">
                 <div className="flex flex-col flex-1 items-center">
                     <h1>HEY, I'M</h1>
-
                     <h1 className="h1-border">SAHAR</h1>
-
                     <div className="flex flex-row items-center mt-3">
                         <h2>A SOFTWARE DEVELOPER</h2>
                     </div>
-
                     <div className="flex flex-row items-center my-4">
                         <SocialLinks />
                     </div>
                 </div>
-
                 <TechIcons />
             </div>
         </div>

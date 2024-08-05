d5d684d9a9569106c6cc6e6842edc1a350e20bd3
Sahar <sahar.bit@gmail.com>
2024-08-05 22:52:41 +0300

    tiny

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Links } from "./Links";
 
 function App() {
   return (
-    <div className="p-6 min-h-screen bg-gray-200">
+    <div className="p-6 min-h-screen" style={{ background: "#FAF9F6" }}>
       <div className="flex flex-col items-center">
         <img
           src={Sahar}
           alt="sahar"
-          className="mb-4 rounded-full shadow-lg w-[170px] border border-black"
+          className="mb-2 rounded-full shadow-lg w-[170px] border border-black"
         />
-        <div className="flex items-center">
+        <div className="flex items-center mb-4">
           <h1 className="font-bold text-2xl">Sahar Biton</h1>
           <img src={Check} alt="check" className="w-5 h-5 ml-1" />
         </div>
